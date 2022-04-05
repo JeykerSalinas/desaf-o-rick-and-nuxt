669eed5968f298c6bde4af8167ebc7e98e1d8495
JeykerSalinas <66428501+JeykerSalinas@users.noreply.github.com>
2022-04-05 18:47:14 -0400

    Setting up environment

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <button @click="increment">Clickme</button>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import mapMutations from 'vuex'
+export default {
+  name: 'IndexPage',
+  methods: {
+    ...mapMutations(['increment']),
+  },
+}
+</script>
+<style scoped></style>
